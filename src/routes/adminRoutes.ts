44b8f3c5b8649fa2045e7b299b5d2e24df32056d
suyan thapa <suyanthapa07@gmail.com>
2025-12-10 15:00:52 +0545

    feat: Add fetchCategories endpoint to retrieve existing categories

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -10,9 +10,12 @@ adminRoutes.get('/user-growth', adminController.getUserGrowth);
 adminRoutes.get('/overview', adminController.getOverview);
 adminRoutes.get('/users/all', adminController.getAllUsers);
 adminRoutes.get('/communities/all', adminController.getAllCommunities);
+//Fetch Caetegories
+adminRoutes.get('/categories', adminController.fetchCategories);
 
 //Add Category for Community
 adminRoutes.post('/addCategory', adminController.addCategoryForCommunity);
+
 // ✅ 2. SPECIFIC PARAMETERIZED ROUTES
 adminRoutes.patch(
   '/users/:id',
